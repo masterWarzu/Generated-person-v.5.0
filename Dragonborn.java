053c8ee16f110d8cc2e64789a827d0b10dce7762
D.E.L <55325944+masterWarzu@users.noreply.github.com>
2022-06-11 00:19:04 +0300

    Add files via upload

--- Dragonborn.java
+++ Dragonborn.java
@@ -1,4 +1,77 @@
 public class Dragonborn
 {
+    String []dragonbornMaleNames = {"Аржхан", "Балазар", "Бхараш", "Геш", "Донаар", "Крив", "Медраш", "Мехен", "Надарр", "Панжет", "Патрин", "Рогар", "Тархун", "Торинн", "Хескан", "Шамаш", "Шединн"};
 
+    String []dragonbornFemaleNames = {"Акра", "Бири", "Даар", "Жери", "Кава", "Коринн", "Мишанн", "Нала", "Перра", "Райанн", "Сора", "Сурина", "Тава", "Уаджит", "Фарида", "Хавилар", "Харанн"};
+
+    String []dragonbornClans = {"Вертизатургьеш", "Даардендриан", "Дельмирев", "Драхедандион", "Кепешкмолик", "Керрилон", "Кимбатуул", "Клеттинтьялор", "Линзакасендалор", "Мьястан", "Неммонис" +
+                                    "Нориксиус", "Офиншталаджиир", "Прексиджандилин", "Турнурот", "Фенкенкабрадон", "Шестенделиат", "Яржерит"};
+
+    Generator generateNumber;
+    GenderGenerated generateGender;
+    AgeGenerated generateAge;
+    SizeGenerated generateSize;
+
+    Dragonborn()
+    {
+        generateNumber = new Generator();
+        generateGender = new GenderGenerated();
+        generateName();
+        generateClan();
+        AgeGenerated.max = 50;
+        AgeGenerated.min = 15;
+        generateAge = new AgeGenerated();
+        generateSize = new SizeGenerated();
+        subraceChoice();
+    }
+
+    String name;
+    void generateName()
+    {
+        if(GenderGenerated.sexChoice.equals("Мужской"))
+            name = dragonbornMaleNames[generateNumber.generate(dragonbornMaleNames.length)];
+        else
+            name = dragonbornFemaleNames[generateNumber.generate(dragonbornFemaleNames.length)];
+    }
+
+    String clan;
+    void generateClan()
+    {
+        clan = dragonbornClans[generateNumber.generate(dragonbornClans.length)];
+    }
+
+    int subRace;
+    void subraceChoice()
+    {
+
+    }
+
+    String subraceOut;
+    void subraceHighElf()
+    {
+
+    }
+
+    void subraceForestElf()
+    {
+
+    }
+
+
+    void dragonbornOutInfo()
+    {
+        System.out.println("Раса: " + RaceGenerated.raceChoice);
+        System.out.println("Подраса: " + subraceOut);
+        System.out.println("Пол: " + GenderGenerated.sexChoice);
+        System.out.println("Имя: " + name);
+        System.out.println("Клан: " + clan);
+        System.out.println("Возраст: " + AgeGenerated.ageChoice);
+        System.out.println("Рост: " + SizeGenerated.sizeChoice);    // переделать генерацию размера
+        System.out.println("Скорость: 30 футов (6 клеток)");
+        System.out.println("Размер: Средний");
+        System.out.println("Класс: " );
+        System.out.println("Оружие: ");
+        System.out.println("Броня: ");
+        System.out.println("Снаряжение: ");
+    }
 }
--- Gnome.java
+++ Gnome.java
@@ -1,4 +1,92 @@
 public class Gnome
 {
+    String []gnomeMaleNames = {"Альвин", "Альстон", "Бёрджелл", "Боддинок", "Брок", "Варрин", "Вренн", "Гербо", "Гимбл", "Глим", "Джебеддо", "Димбл", "Зук", "Келлен", "Намфудль", "Оррин", "Рундар" +
+                                "Сибо","Синдри", "Фонкин", "Фраг", "Эльдон", "Эрки"};
 
+    String []gnomeFemaleNames = {"Бимпноттин", "Брина", "Вейвокет", "Донелла", "Думавиль", "Занна", "Карамип", "Карлин", "Лилли", "Лорилла", "Лупмоттин", "Марднаб", "Никс", "Нисса", "Ода", "Орла" +
+                                    "Ройвин","Тана", "Шамилла", "Элла", "Элливик", "Эллиджобелл"};
+
+    String []gnomeClans = {"Берен", "Гаррик", "Дергель", "Мёрнинг", "Некль", "Нингель", "Рольнор", "Тимберс", "Турен", "Фолькор", "Щеппен"};
+
+    String []gnomeNicknames = {"Барсук", "Блестяшка", "Двухзамок", "Ку", "Ним", "Одна-Туфля", "Оспина", "Пепельник", "Пивохлёб", "Плащик", "Тестохват", "Утковал", "Фниппер"};
+
+    Generator generateNumber;
+    GenderGenerated generateGender;
+    AgeGenerated generateAge;
+    SizeGenerated generateSize;
+
+    Gnome()
+    {
+        generateNumber = new Generator();
+        generateGender = new GenderGenerated();
+        generateName();
+        generateClan();
+        generateNickname();
+        AgeGenerated.max = 400;
+        AgeGenerated.min = 50;
+        generateAge = new AgeGenerated();
+        generateSize = new SizeGenerated();
+        subraceChoice();
+    }
+
+    String name;
+    void generateName()
+    {
+        if(GenderGenerated.sexChoice.equals("Мужской"))
+            name = gnomeMaleNames[generateNumber.generate(gnomeMaleNames.length)];
+        else
+            name = gnomeFemaleNames[generateNumber.generate(gnomeFemaleNames.length)];
+    }
+
+    String clan;
+    void generateClan()
+    {
+        clan = gnomeClans[generateNumber.generate(gnomeClans.length)];
+    }
+
+    String nickname;
+    void generateNickname()
+    {
+        nickname = gnomeNicknames[generateNumber.generate(gnomeNicknames.length)];
+    }
+
+    int subRace;
+    void subraceChoice()
+    {
+        subRace = generateNumber.generate(2);
+        if(subRace == 1)
+            subraceHighElf();
+        else
+            subraceForestElf();
+    }
+
+    String subraceOut;
+    void subraceHighElf()
+    {
+        subraceOut = "Лесной гном";
+    }
+
+    void subraceForestElf()
+    {
+        subraceOut = "Скальный гном";
+    }
+
+
+    void gnomeOutInfo()
+    {
+        System.out.println("Раса: " + RaceGenerated.raceChoice);
+        System.out.println("Подраса: " + subraceOut);
+        System.out.println("Пол: " + GenderGenerated.sexChoice);
+        System.out.println("Имя: " + name);
+        System.out.println("Клан: " + clan);
+        System.out.println("Прозвище: " + nickname);
+        System.out.println("Возраст: " + AgeGenerated.ageChoice);
+        System.out.println("Рост: " + SizeGenerated.sizeChoice);    // переделать генерацию размера
+        System.out.println("Скорость: 25 футов (5 клеток)");
+        System.out.println("Размер: Средний");
+        System.out.println("Класс: " );
+        System.out.println("Оружие: ");
+        System.out.println("Броня: ");
+        System.out.println("Снаряжение: ");
+    }
 }
